53402cfe9c854e1e36671890bc3d93f39c579661
Nguyen Minh DUc <ngmductm@gmail.com>
2020-09-04 19:14:15 +0700

    add game and update hangman

--- Code/Contact.js
+++ Code/Contact.js
@@ -1,33 +1,33 @@
 let modal = document.getElementsByClassName('modal');
 let btn = document.getElementsByClassName('btn');
 let span = document.getElementsByClassName('close');
-btn[0].onclick = function () {
+btn[0].onclick = function() {
     modal[0].style.display = "block";
 }
-span[0].onclick = function () {
+span[0].onclick = function() {
     modal[0].style.display = "none";
 }
-btn[1].onclick = function () {
+btn[1].onclick = function() {
     modal[1].style.display = "block";
 }
-span[1].onclick = function () {
+span[1].onclick = function() {
     modal[1].style.display = "none";
 }
-btn[2].onclick = function () {
+btn[2].onclick = function() {
     modal[2].style.display = "block";
 }
-span[2].onclick = function () {
+span[2].onclick = function() {
     modal[2].style.display = "none";
 }
-btn[3].onclick = function () {
+btn[3].onclick = function() {
     modal[3].style.display = "block";
 }
-span[3].onclick = function () {
+span[3].onclick = function() {
     modal[3].style.display = "none";
 }
-btn[4].onclick = function () {
+btn[4].onclick = function() {
     modal[4].style.display = "block";
 }
-span[4].onclick = function () {
+span[4].onclick = function() {
     modal[4].style.display = "none";
 }
